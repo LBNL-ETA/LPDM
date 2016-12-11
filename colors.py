1768175bc21927a89a2cbbdb08ab3acbf0bd9d38
bob <sczarnecki@lbl.gov>
2016-12-11 15:23:45 -0800

    Adding copyright header to top of py files.

--- colors.py
+++ colors.py
@@ -1,4 +1,17 @@
 
+
+################################################################################################################################
+# *** Copyright Notice ***
+#
+# "Price Based Local Power Distribution Management System (Local Power Distribution Manager) v1.0" 
+# Copyright (c) 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory 
+# (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+# If you have questions about your rights to use or distribute this software, please contact 
+# Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+################################################################################################################################
+
+
 class Colors:
     BLACK = '\033[0;30m'
     RED = '\033[0;31m'
--- tug_devices/fan_eud.py
+++ tug_devices/fan_eud.py
@@ -1,3 +1,16 @@
+
+
+################################################################################################################################
+# *** Copyright Notice ***
+#
+# "Price Based Local Power Distribution Management System (Local Power Distribution Manager) v1.0" 
+# Copyright (c) 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory 
+# (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+# If you have questions about your rights to use or distribute this software, please contact 
+# Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+################################################################################################################################
+
 """
 Implementation of a PWM and beaglebone based EUD device.
 """

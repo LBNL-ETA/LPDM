bec4e2243e7e87c866fbd9feaef695da0c340bfe
Michael Spears <mspears@lbl.gov>
2017-02-28 19:43:30 -0800

    updates/new supervisor events

--- supervisor/lpdm_event/__init__.py
+++ supervisor/lpdm_event/__init__.py
@@ -3,4 +3,7 @@ from lpdm_price_event import LpdmPriceEvent
 from lpdm_ttie_event import LpdmTtieEvent
 from lpdm_init_event import LpdmInitEvent
 from lpdm_kill_event import LpdmKillEvent
+from lpdm_connect_device_event import LpdmConnectDeviceEvent
+from lpdm_assign_grid_controller_event import LpdmAssignGridControllerEvent
+from lpdm_run_time_error_event import LpdmRunTimeErrorEvent
 import event_type
--- supervisor/lpdm_event/lpdm_power_event.py
+++ supervisor/lpdm_event/lpdm_power_event.py
@@ -1,6 +1,6 @@
 from lpdm_base_event import LpdmBaseEvent
 
 class LpdmPowerEvent(LpdmBaseEvent):
-    def __init__(self, source_device_id, target_device_id, value):
-        LpdmBaseEvent.__init__(self, source_device_id, target_device_id, value)
+    def __init__(self, source_device_id, target_device_id, time, value):
+        LpdmBaseEvent.__init__(self, source_device_id, target_device_id, time, value)
         self.event_type = "power"

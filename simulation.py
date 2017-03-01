5ddde3842723152821a55d01d0c086147510778a
Michael Spears <mspears@lbl.gov>
2017-02-28 19:51:37 -0800

    updates to the simulation runner

--- simulation.py
+++ simulation.py
@@ -11,33 +11,33 @@
 # Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
 ################################################################################################################################
 
-from supervisor import Supervisor
-from tug_devices.grid_controller import GridController
-from tug_devices.eud import Eud
-from tug_devices.light import Light
-from tug_devices.insight_eud import InsightEud
-from tug_devices.diesel_generator import DieselGenerator
-from tug_devices.fan_eud import PWMfan_eud
-from tug_devices.refrigerator import Refrigerator
-from tug_devices.air_conditioner import AirConditioner
-from messenger import Messenger
-from simulation_logger import SimulationLogger
-import logging
 import os
 import re
 import pprint
 import random
 import json
-import urllib2
+import logging
+from supervisor import Supervisor
+from simulation_logger import SimulationLogger
+# from device.diesel_generator import DieselGenerator
+# from tug_devices.grid_controller import GridController
+# from tug_devices.eud import Eud
+# from tug_devices.light import Light
+# from tug_devices.insight_eud import InsightEud
+# from tug_devices.diesel_generator import DieselGenerator
+# from tug_devices.fan_eud import PWMfan_eud
+# from tug_devices.refrigerator import Refrigerator
+# from tug_devices.air_conditioner import AirConditioner
+
 
 class TugSimulation:
-    def __init__(self, params=None):
-        # self.logger = logging.getLogger('{}.{}'.format(logger_setup.APP_NAME, __name__))
-        verbose_logging = True if type(params) is dict and params["verbose_logging"] else False
-
-        # setup the log manager
-        self.simulation_log_manager = SimulationLogger(verbose_logging)
-        self.simulation_log_manager.initializeLogging()
+    def __init__(self, params):
+        # setup the logger
+        self.simulation_log_manager = SimulationLogger(
+            console_log_level=params.get("console_log_level", logging.DEBUG),
+            file_log_level=params.get("file_log_level", logging.DEBUG)
+        )
+        self.simulation_log_manager.init()
 
         self.simulation_id = self.simulation_log_manager.log_id
         self.logger = self.simulation_log_manager.logger
@@ -49,27 +49,18 @@ class TugSimulation:
         self.temperature_controller = None
 
         self.current_time = None
-        self.end_time = int(params["run_time_days"]) * 60 * 60 * 24 if params and 'run_time_days' in params.keys() and params['run_time_days'] else 60 * 60 * 24 * 7
-        self.status_interval = None
 
-        self.server_ip = params["server_ip"] if params and 'server_ip' in params.keys() else  None
-        self.server_port = params["server_port"] if params and 'server_port' in params.keys() else None
-        self.client_id = params["client_id"] if params and 'client_id' in params.keys() else None
-        self.socket_id = params["socket_id"] if params and 'socket_id' in params.keys() else None
-
-        # simulation doesn't try and send results to the dashboard
-        self.headless = params["headless"] if params and params.has_key("headless") else False
+        # setup the run_time for the simulation
+        run_time_days = params.get("run_time_days", 7)
+        self.end_time = run_time_days * 60 * 60 * 24
 
         # force the devices to ignore changes in price
-        self.static_price = True if params and params.has_key('static_price') and params["static_price"] else False
+        self.static_price = params.get("static_price", False)
 
-        self.last_dump = None
-
-        self.device_status = []
         self.device_info = []
-        self.simulations = []
 
-        self.initializeSimulation(params)
+    def init(self, config):
+        """Initialize the devices"""
 
     def initializeSimulation(self, params):
 
@@ -209,88 +200,6 @@ class TugSimulation:
                     new_dict[prop] = config[prop]
         return new_dict
 
-    # def defaultDieselGenerator(self):
-    #     return {
-    #         "device_name": "diesel_generator",
-    #         "config_time": 10,
-    #         "price": 1.0,
-    #         "fuel_tank_capacity": 100.0,
-    #         "fuel_level": 100.0,
-    #         "fuel_reserve": 20.0,
-    #         "days_to_refuel": 7,
-    #         "kwh_per_gallon": 36.36,
-    #         "time_to_reassess_fuel": 21600,
-    #         "fuel_price_change_rate": 5,
-    #         "capacity": 2000.0,
-    #         "gen_eff_zero": 25,
-    #         "gen_eff_100": 40,
-    #         "price_reassess_time": 60,
-    #         "fuel_base_cost": 5
-    #     }
-
-    # def defaultEudFan(self):
-    #     return {
-    #         "device_name": "EUD - fan",
-    #         "max_power_use": 15000.0,
-    #         "schedule": [['0300', 1], ['2300', 0]]
-    #     }
-
-    # def defaultGridController(self):
-    #     return {
-    #         "device_name": "Grid Controller",
-    #         "capacity": 3000.0,
-    #         "connected_devices": [],
-    #         "check_battery_soc_rate": 60 * 5,
-    #         "battery_config": {
-    #             "capacity": 5.0,
-    #             "min_soc": 0.20,
-    #             "max_soc": 0.80,
-    #             "max_charge_rate": 1000.0,
-    #             "roundtrip_eff": 0.9
-    #         }
-    #     }
-
-    def signalSimulationStart(self):
-        if self.headless:
-            return True
-        else:
-            try:
-                req = urllib2.Request('http://{0}:{1}/api/simulation_start'.format(self.server_ip, self.server_port))
-                req.add_header('Content-Type', 'application/json')
-                response = urllib2.urlopen(req, json.dumps({"client_id": self.client_id, "socket_id": self.socket_id}))
-                return True
-            except:
-                self.logger.exception("Unable to connect to web client for simulation {}".format(self.simulation_id))
-                return False
-
-    def signalSimulationEvent(self, data):
-        if self.headless:
-            return True
-        else:
-            try:
-                data["socket_id"] = self.socket_id
-                data["client_id"] = self.client_id
-                req = urllib2.Request('http://{0}:{1}/api/simulation_event'.format(self.server_ip, self.server_port))
-                req.add_header('Content-Type', 'application/json')
-                response = urllib2.urlopen(req, json.dumps(data))
-                return True
-            except:
-                self.logger.exception("Unable to connect to web client for simulation {}".format(self.simulation_id))
-                return False
-
-    def signalSimulationEnd(self):
-        if self.headless:
-            return True
-        else:
-            try:
-                req = urllib2.Request('http://{0}:{1}/api/simulation_end'.format(self.server_ip, self.server_port))
-                req.add_header('Content-Type', 'application/json')
-                response = urllib2.urlopen(req, json.dumps({"client_id": self.client_id, "socket_id": self.socket_id}))
-                return True
-            except:
-                self.logger.exception("Unable to connect to web client for simulation {}".format(self.simulation_id))
-                return False
-
     def finishSimulation(self):
         for dev in self.eud_devices:
             dev.finish()
@@ -304,15 +213,29 @@ class TugSimulation:
 
 
     def run(self):
-        self.logger.info('run simulation for {} days'.format(self.end_time / 60.0 / 60.0 / 24.0))
-        if self.signalSimulationStart():
-            for self.current_time in range(0, self.end_time):
-                self.messenger.changeTime(self.current_time)
+        supervisor = Supervisor()
+        supervisor.add_device("device_1", Device, {"device_id": "device_1"})
+        supervisor.add_device("device_2", Device, {"device_id": "device_2"})
+        supervisor.run_simulation()
 
-            self.finishSimulation()
-            self.signalSimulationEnd()
-            self.logger.info("Simulation #{} finished".format(self.simulation_id))
-        else:
-            self.logger.exception("Unable to connect to web client for simulation {}".format(self.simulation_id))
+if __name__ == "__main__":
+    with open("scenarios/scenario.json") as config_file:
+        config = json.load(config_file)
+    print config
+    # setup logging for the app
+    log_manager = SimulationLogger(
+        console_log_level=config.get("console_log_level", logging.DEBUG),
+        file_log_level=config.get("file_log_level", logging.DEBUG),
+        pg_log_level=config.get("pg_log_level", logging.DEBUG)
+    )
+    print "init log manager"
+    log_manager.init()
+    print "done"
 
+
+    supervisor = Supervisor()
+    supervisor.load_config(config)
+
+    log_manager.logger.info("run the simulation")
+    supervisor.run_simulation()
 

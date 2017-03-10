0687f0ed1f1b6b1f836c3d1480e146c8b1e32b53
Michael Spears <mspears@lbl.gov>
2017-03-10 14:28:52 -0800

    updated default values for capacity and price

--- device/grid_controller/power_source_manager/power_source_item.py
+++ device/grid_controller/power_source_manager/power_source_item.py
@@ -2,7 +2,7 @@ class PowerSourceItem:
     def __init__(self, device_id, DeviceClass, capacity=None, load=None, price=None):
         self.device_id = device_id
         self.DeviceClass = DeviceClass
-        self.capacity = 0.0
+        self.capacity = None
         self.load = 0.0
-        self.price = 0.0
+        self.price = None
 

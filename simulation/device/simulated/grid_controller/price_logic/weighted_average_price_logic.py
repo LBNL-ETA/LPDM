f218538c77460551a29618d43c9b6fbbef44c4f3
Michael Spears <mspears@lbl.gov>
2017-04-30 07:28:07 -0700

    fixed weighted average price calc to compare a power source's load to the overall load, instead of capacity

--- simulation/device/simulated/grid_controller/price_logic/weighted_average_price_logic.py
+++ simulation/device/simulated/grid_controller/price_logic/weighted_average_price_logic.py
@@ -4,6 +4,8 @@ To use, set the price_logic_class key in the grid controller to
 the name of the class, which in this case is AveragePrice
 """
 
+from device.simulated.utility_meter import UtilityMeter
+
 class WeightedAveragePriceLogic(object):
     """
     Calculates the average price of all available power sources,
@@ -17,12 +19,13 @@ class WeightedAveragePriceLogic(object):
         numerator = 0.0
         denominator = 0.0
         power_sources = self.power_source_manager.get_available_power_sources()
+        total_load = self.power_source_manager.total_load()
         if len(power_sources):
             power_sources.sort(lambda a, b: cmp(a.price, b.price))
             the_price = 0.0
             for ps in power_sources:
                 if ps.load > 0 and ps.capacity > 0:
-                    the_price += (ps.load / ps.capacity) * ps.price
+                    the_price += (ps.load / total_load) * ps.price
             if the_price < power_sources[0].price:
                 # price can't be cheaper than the cheapest price
                 return power_sources[0].price
